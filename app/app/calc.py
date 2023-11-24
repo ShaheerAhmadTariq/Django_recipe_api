38f34f05ca4b520882a159ceb74cf6203d5e3fcb
ShaheerAhmadTariq <shaheerahmadtariq@gmail.com>
2023-11-24 16:28:48 +0500

    Feat: Installed Postgress in DockerFile and Perform basic testing

---
+++ app/app/calc.py
@@ -0,0 +1,9 @@
+# simple add function 
+
+def add_numbers(x, y):
+    """Add two numbers together"""
+    return x + y
+
+def subtract_numbers(x, y):
+    """Subtract x from y and return value"""
+    return y - x
---
+++ app/app/tests.py
@@ -0,0 +1,16 @@
+"""
+Sample test file
+"""
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+
+    def test_add_numbers(self):
+        """Test that two numbers are added together"""
+        self.assertEqual(calc.add_numbers(3, 8), 11)
+
+    def test_subtract_numbers(self):
+        """Test that values are subtracted and returned"""
+        self.assertEqual(calc.subtract_numbers(11, 5), 6)
